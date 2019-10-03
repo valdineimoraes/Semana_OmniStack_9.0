18edd8f5b22a46be6f9597236695e3f5fb092c50
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2019-10-03 20:01:55 -0300

    add routes ao projeto

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World my friend'});
-});
+// GET, Post, Put, Delete
+//req.params = Acessar route params (para edição, delete)
+//req.body = Acessar corpo da requisicao (oara criacao, edicao)
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333);
